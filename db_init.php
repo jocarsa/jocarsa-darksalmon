b4f02ef8e956186ebf992eee4e63376e2d72e6a3
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-04-02 13:14:08 +0200

    1

--- db_init.php
+++ db_init.php
@@ -10,7 +10,7 @@
 
 function getPDOConnection(): PDO
 {
-    $dbFile = __DIR__ . '/data.db';
+    $dbFile = __DIR__ . '../databases/darksalmon.db';
     $dbExisted = file_exists($dbFile);
 
     try {
